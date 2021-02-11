ccb98cb840b9d9576a8283e6bfecc87b37ed0072
Elina Sime <elina.sime@gmail.com>
2021-02-11 21:26:41 +0200

    Ficity function changed

--- src/main/scala/FunctionExercises.scala
+++ src/main/scala/FunctionExercises.scala
@@ -1,16 +1,15 @@
 object FunctionExercises extends App{
 
   def isPalindrome(text:String): Boolean = {
-    //TODO actually check if the text reads the same from both ways
     val textWhitespacesRemoved = text.replace(" ", "")
-
+    //textWhitespacesRemoved.equalsIgnoreCase(textWhitespacesRemoved.reverse))
     if (textWhitespacesRemoved.equalsIgnoreCase(textWhitespacesRemoved.reverse))
-    {
-      println(s"'$text' is a palindrome")
+   {
+     println(s"'$text' is a palindrome")
       true
     }
     else {
-      println(s"'$text' is not a palindrome")
+     println(s"'$text' is not a palindrome")
       false
     }
   }
@@ -25,10 +24,9 @@ object FunctionExercises extends App{
       counter = -1
     }
     else {
-      while(yearlyResult != p){
-        var yearlyResultNew = yearlyResult * perc/100.0 + delta
+      while(yearlyResult < p && counter >= 0){
+        yearlyResult += yearlyResult * perc/100.0 + delta
         counter += 1
-        yearlyResult = yearlyResultNew.ceil.toInt
       }
     }
     counter
@@ -42,11 +40,8 @@ object FunctionExercises extends App{
   //also consider that it is possible that city never reaches p, then you should return -1
   //hint: if population does not increase after first year it will never reach p
 
-  println(getCityYear(1000,2,50, 1200))
-  println(getCityYear(1000,2,-50, 1200))
-  println(getCityYear(1500000,2.5,10000, 2000000))
 
-  //  println(getCityYear(1000,2,50,1200)) should print 3
-  //  println(getCityYear(1000,2,-50,1200)) should print -1
-  //  println(getCityYear(1500000,2.5,10000,2000000)) should print 10
+  println(getCityYear(1000,2,50,1200)) //should print 3
+  println(getCityYear(1000,2,-50,1200)) //should print -1
+  println(getCityYear(1500000,2.5,10000,2000000)) //should print 10
 }
