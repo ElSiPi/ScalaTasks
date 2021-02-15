10d3c7a3b4030f68508004395b74b7fbd3ae8f4b
Elina Sime <elina.sime@gmail.com>
2021-02-15 19:54:03 +0200

    Function exercise fixed and first made shorter

--- src/main/scala/FunctionExercises.scala
+++ src/main/scala/FunctionExercises.scala
@@ -2,34 +2,24 @@ object FunctionExercises extends App{
 
   def isPalindrome(text:String): Boolean = {
     val textWhitespacesRemoved = text.replace(" ", "")
-    //textWhitespacesRemoved.equalsIgnoreCase(textWhitespacesRemoved.reverse))
-    if (textWhitespacesRemoved.equalsIgnoreCase(textWhitespacesRemoved.reverse))
-   {
-     println(s"'$text' is a palindrome")
-      true
-    }
-    else {
-     println(s"'$text' is not a palindrome")
-      false
-    }
+    textWhitespacesRemoved.equalsIgnoreCase(textWhitespacesRemoved.reverse))
+
   }
   isPalindrome("This is a palindrome")
   isPalindrome("Borrow or rob")
   isPalindrome("Taco cat")
-
+  //check it and correct it and commit it
   def getCityYear(p0:Int, perc:Double, delta:Int, p:Int): Int = {
-    var counter = 0
-    var yearlyResult = p0 * perc/100.00 + delta
-    if(yearlyResult < p0) {
-      counter = -1
+
+    var yearlyResult = p0 + (p0 * perc/100) + delta
+    var years = 1
+    if(yearlyResult <= p0) years = -1
+    while(yearlyResult<=p && years>0) {
+      var newYearlyResult = yearlyResult + (yearlyResult * perc/100) + delta
+      yearlyResult = newYearlyResult
+      years += 1
     }
-    else {
-      while(yearlyResult < p && counter >= 0){
-        yearlyResult += yearlyResult * perc/100.0 + delta
-        counter += 1
-      }
-    }
-    counter
+    years
   }
   //Write a function which takes 4 parameters p0, perc, delta, p
   //return integer of years when the population reaches p
@@ -41,7 +31,7 @@ object FunctionExercises extends App{
   //hint: if population does not increase after first year it will never reach p
 
 
-  println(getCityYear(1000,2,50,1200)) //should print 3
+  println(getCityYear(1000,2,50, 1200)) //should print 3
   println(getCityYear(1000,2,-50,1200)) //should print -1
-  println(getCityYear(1500000,2.5,10000,2000000)) //should print 10
+  println(getCityYear(1500000,2.5,10000, 2000000)) //should print 10
 }
