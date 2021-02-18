562c465dd711755c910439bda952ac960a84d9bd
Elina Sime <elina.sime@gmail.com>
2021-02-18 20:51:26 +0200

    Get Common exercise sets

---
+++ src/main/scala/GetCommon.scala
@@ -0,0 +1,29 @@
+import scala.math.Ordering.Implicits.seqOrdering
+
+object GetCommon extends App{
+
+  def getCommonElements(s1: Seq[Int], s2: Seq[Int], s3: Seq[Int]): Seq[Int] = {
+    //TODO return sorted Sequence of common elements in all 3 sequences
+    val set1 = s1.toSet
+    val set2 = s2.toSet
+    val set3 = s3.toSet
+    val commonElem = (set1 & set2 & set3).toSeq.sorted
+    commonElem
+
+  }
+  val testSeq1 = Seq(1,3,5,7,9)
+  val testSeq2 = Seq(3,1,4,6,8,0)
+  val testSeq3 = Seq(14,16,1,18,10,3)
+  val testSeq4 = getCommonElements(testSeq1, testSeq2, testSeq3)
+  println(testSeq4)
+
+  def isPangram(text: String, alphabet : String = "abcdefghijklmnopqrstuvwxyz"): Boolean= {
+    //TODO determine if the sentence contains all English letters
+    //https://en.wikipedia.org/wiki/Pangram
+    // (case is not important here)
+    //this function should work on other languages too, if we pass it different alphabet
+    false
+  }
+
+  println(isPangram("The five boxing wizards jump quickly.")) //should be true
+}
